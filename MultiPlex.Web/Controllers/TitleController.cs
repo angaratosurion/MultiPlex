533aba6e72a80fcbd0b206658c1144c0685eb6cb
angaratosurion <angarato.surion@gmail.com>
2015-10-02 20:17:24 +0300

    started creatign views
    started creatign views

--- MultiPlex.Web/Controllers/TitleController.cs
+++ MultiPlex.Web/Controllers/TitleController.cs
@@ -13,93 +13,93 @@ using MultiPlex.Core.Managers;
 namespace MultiPlex.Web.Controllers
 {
 
-    [Export("Title", typeof(IController))]
-    [PartCreationPolicy(CreationPolicy.NonShared)]
-    public class TitleController : Controller
-    {
-        TitleManager tmngr = new TitleManager();
-        // GET: Category
-        //public ActionResult Index(string wid, int catid)
-        //{
-        //    try
-        //    {
-        //        if (CommonTools.isEmpty(wid))
-        //        {
-        //            //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //[Export("Title", typeof(IController))]
+    //[PartCreationPolicy(CreationPolicy.NonShared)]
+    //public class TitleController : Controller
+    //{
+    //    TitleManager tmngr = new TitleManager();
+    //    // GET: Category
+    //    //public ActionResult Index(string wid, int catid)
+    //    //{
+    //    //    try
+    //    //    {
+    //    //        if (CommonTools.isEmpty(wid))
+    //    //        {
+    //    //            //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-        //        }
-        //        Category cat=null;
-        //        if (cat == null)
-        //        {
-        //            return HttpNotFound();
-        //        }
-        //        return View(cat);
-        //    }
-        //    catch (Exception ex)
-        //    {
+    //    //        }
+    //    //        Category cat=null;
+    //    //        if (cat == null)
+    //    //        {
+    //    //            return HttpNotFound();
+    //    //        }
+    //    //        return View(cat);
+    //    //    }
+    //    //    catch (Exception ex)
+    //    //    {
 
-        //        CommonTools.ErrorReporting(ex);
-        //        return null;
-        //    }
-        //}
-        public ActionResult Index(string wikiname, int catid)
-        {
-            try
-            {
-                if (CommonTools.isEmpty(wikiname))
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+    //    //        CommonTools.ErrorReporting(ex);
+    //    //        return null;
+    //    //    }
+    //    //}
+    //    public ActionResult Index(string wikiname, int catid)
+    //    {
+    //        try
+    //        {
+    //            if (CommonTools.isEmpty(wikiname))
+    //            {
+    //                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
 
-                }
-                List<Title> titles = this.tmngr.GetTitlesbyCategory(wikiname, catid);
-                if (titles == null)
-                {
-                    return HttpNotFound();
-                }
-                return View(titles);
-            }
-            catch (Exception ex)
-            {
+    //            }
+    //            List<Title> titles = this.tmngr.GetTitlesbyCategory(wikiname, catid);
+    //            if (titles == null)
+    //            {
+    //                return HttpNotFound();
+    //            }
+    //            return View(titles);
+    //        }
+    //        catch (Exception ex)
+    //        {
 
-                CommonTools.ErrorReporting(ex);
-                return null;
-            }
-        }
-        //    [Authorize]
-        //    public ActionResult Create()
-        //    {
-        //        try
-        //        {
-        //            return View();
-        //        }
-        //        catch (Exception ex)
-        //        {
+    //            CommonTools.ErrorReporting(ex);
+    //            return null;
+    //        }
+    //    }
+    //    //    [Authorize]
+    //    //    public ActionResult Create()
+    //    //    {
+    //    //        try
+    //    //        {
+    //    //            return View();
+    //    //        }
+    //    //        catch (Exception ex)
+    //    //        {
 
-        //            CommonTools.ErrorReporting(ex);
-        //            return null;
-        //        }
+    //    //            CommonTools.ErrorReporting(ex);
+    //    //            return null;
+    //    //        }
 
-        //    }
+    //    //    }
 
-        //    [HttpPost]
-        //    [ValidateAntiForgeryToken]
-        //    public ActionResult Create(Title title)
-        //    {
-        //        try
-        //        {
-        //            if (ModelState.IsValid)
-        //            {
+    //    //    [HttpPost]
+    //    //    [ValidateAntiForgeryToken]
+    //    //    public ActionResult Create(Title title)
+    //    //    {
+    //    //        try
+    //    //        {
+    //    //            if (ModelState.IsValid)
+    //    //            {
 
-        //            }
-        //            return title;
-        //        }
-        //        catch (Exception ex)
-        //        {
+    //    //            }
+    //    //            return title;
+    //    //        }
+    //    //        catch (Exception ex)
+    //    //        {
 
-        //            CommonTools.ErrorReporting(ex);
-        //            return null;
-        //        }
-        //    }
-        //}
-    }
+    //    //            CommonTools.ErrorReporting(ex);
+    //    //            return null;
+    //    //        }
+    //    //    }
+    //    //}
+    //}
 }
